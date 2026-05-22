55d68959869fd78e4c037361fb2608ba44743d1c
kit sitou <ctwohofa@gmail.com>
2026-05-21 17:34:21 -0700

    Initial Commit

---
+++ LoginCalculatorClass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginCalculatorClass
+//
+//  Created by Kit Sitou on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginCalculatorClass/LoginCalculatorClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginCalculatorClassApp.swift
+//  LoginCalculatorClass
+//
+//  Created by Kit Sitou on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginCalculatorClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
